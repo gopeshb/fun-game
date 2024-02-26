d1e7e2def503da207b95cf8729580c60025c6d07
gopeshb <gopeshbhatia01@outlook.com>
2024-02-26 22:01:33 +0530

    game done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -61,7 +61,7 @@ export const signin=async (req,res,next)=>{
         }
         const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
         const {password:pass,...rest}=validUser._doc;
-        res.cookie('access_token',token,{httpOnly:true,expires: new Date(Date.now()+24*60*60*1000)})
+        res.cookie('access_token',token,{httpOnly:true})
         .status(200).json(rest);
     } catch (error) {
         console.error(error);
